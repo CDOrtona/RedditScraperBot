3c96760b94f9bcf71036e7cb84d982d8eb0d739b
Durton24 <cris.dortona@gmail.com>
2021-01-27 16:09:08 +0100

    minor changes

--- src/main/java/RedditPics.java
+++ src/main/java/RedditPics.java
@@ -28,7 +28,8 @@ public class RedditPics extends TelegramLongPollingBot {
     }
 
     public void onUpdateReceived(Update update) {
-
+//adsfasfasd
+        
         Message message;
         String choosenSub = null;
         int numImages = 0;
